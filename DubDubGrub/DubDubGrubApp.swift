f81a93561b013d5ac6278e7186fc103b40988a0d
Farangis <farangis98@ya.ru>
2023-07-12 14:45:24 +0500

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by IMacIBT1 on 12/07/23.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
